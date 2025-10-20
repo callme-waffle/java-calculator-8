352aeceeadb6d71f7e4383ee2d3b0852e8cab649
nate2402 <nate2402@ajou.ac.kr>
2025-10-20 20:51:29 +0900

    test(functions): 함수 예외처리 테스트케이스 추가

--- src/test/java/calculator/functions/CalculatorFunctionAdapterTest.java
+++ src/test/java/calculator/functions/CalculatorFunctionAdapterTest.java
@@ -18,19 +18,38 @@ class CalculatorFunctionAdapterTest {
 	}
 
 	@Test
-	void detectNumbers1FromRange() {
+	void detectNumbersFromRangeTrueCase1() {
 		calc.detectNumbers("1,2,3,4,5");
 		assertThat(calc.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5));
 	}
 
 	@Test
-	void detectNumbers2FromRange() {
+	void detectNumbersFromRangeTrueCase2() {
 		calc.detectNumbers("1:2:3:4:5");
 		assertThat(calc.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5));
 	}
+
 	@Test
-	void detectNumbers3FromRange() {
+	void detectNumbersFromRangeTrueCase3() {
 		calc.detectNumbers("1:2,3:4,5");
 		assertThat(calc.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5));
+	}
+
+	@Test
+	void detectNumbersFromRangeFalseCase1() {
+		assertThatThrownBy(() -> calc.detectNumbers("1,2,3,4,"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void detectNumbersFromRangeFalseCase2() {
+		assertThatThrownBy(() -> calc.detectNumbers("1,2,3,4,,5"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void detectNumbersFromRangeFalseCase3() {
+		assertThatThrownBy(() -> calc.detectNumbers(",,1,2,3,4,5"))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
